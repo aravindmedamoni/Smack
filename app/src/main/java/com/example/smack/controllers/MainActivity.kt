0db0f65bccb4920ef6491f8967cb41b74e46cd5c
aravindmedamoni <aravnd.accurate@gmail.com>
2020-03-14 22:42:25 +0530

    channel dialog created..

--- app/src/main/java/com/example/smack/controllers/MainActivity.kt
+++ app/src/main/java/com/example/smack/controllers/MainActivity.kt
@@ -7,7 +7,9 @@ import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -17,7 +19,9 @@ import com.example.smack.services.AuthService
 import com.example.smack.services.UserDataService
 import com.example.smack.utils.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_channel_dialog.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +33,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
      //   val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        //hide keyboard
+        hideKeyBoard()
 
         val toggle = ActionBarDrawerToggle(
             this,drawer_layout,toolbar,
@@ -75,13 +81,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onAddChannelButtonClicked(view : View){
+        if(AuthService.isLogedIn){
+            val builder = AlertDialog.Builder(this)
+            val dialogView = layoutInflater.inflate(R.layout.add_channel_dialog,null)
+            builder.setView(dialogView)
+                .setPositiveButton("Add"){ dialogInterface,i ->
+                    //add some logic here to display getting content from the user
+                    val channelName = channelNameEditText.text.toString()
+                    val channelDescription = channelDescEditText.text.toString()
 
+                    //create channel with name and description
+                    hideKeyBoard()
+                }
+                .setNegativeButton("Cancel"){
+                    dialogInterface ,i ->
+                    //add logic for cancel and close the window
+                    hideKeyBoard()
+                }
+                .show()
+        }
+    }
+
+    private fun hideKeyBoard(){
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if(inputManager.isAcceptingText){
+            inputManager.hideSoftInputFromWindow(currentFocus!!.windowToken,0)
+        }
     }
 
     fun onSendButtonClicked(view : View){
 
     }
-    fun navHeaderSetToDefault(){
+   private fun navHeaderSetToDefault(){
         navHeaderLoginButton.text = "Login"
         navHeaderProfilerName.text = "Please Login"
         navHeaderprofilerMail.text = ""
